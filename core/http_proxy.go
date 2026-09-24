aad8619b135c01f6a93ce8d9bc8e8c3b0853b76d
musty <mustaphaabdulaziz001@gmail.com>
2026-09-25 00:53:54 +0100

    Remove unused go-vhost import

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -39,7 +39,6 @@ import (
 	"github.com/elazarl/goproxy"
 	"github.com/fatih/color"
 	"github.com/go-acme/lego/v3/challenge/tlsalpn01"
-	"github.com/inconshreveable/go-vhost"
 	http_dialer "github.com/mwitkow/go-http-dialer"
 
 	"github.com/x-tymus/x-tymus/database"
